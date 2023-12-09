7fdf42629557325b322d6cfd79c2741d9ba67652
VinneyPooh <soni.zarana@gmail.com>
2023-12-09 02:54:18 +0000

    parabank hw

--- parabank-sw3/src/testsuit/loginTest.java
+++ parabank-sw3/src/testsuit/loginTest.java
@@ -1,5 +1,33 @@
 package testsuit;
 
-public class loginTest {
+import browserfactory.BaseTest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+public class loginTest extends BaseTest {
+    String baseUrl="https://parabank.parasoft.com/parabank/index.htm";
+    @Before
+    public void setUp(){
+        openBrowser(baseUrl);
+    }
+    @Test
+    //2.verifyTheErrorMessage
+    //* Enter invalid username
+    //* Enter invalid password
+    //* Click on Login button
+    //* Verify the error message ‘The username and password could not be verified.’
+    public void verifyTheErrorMessage(){
+        WebElement uName= driver.findElement(By.name("username"));
+        uName.sendKeys("zara");
+        WebElement pW=driver.findElement(By.name("password"));
+        pW.sendKeys("123@123@123");
+        driver.findElement(By.xpath("//form[@name='login']/div[3]/input")).click();
+        String actualText = driver.findElement(By.xpath("//div[@id='rightPanel']/p")).getText();
+        String expectedText = "An internal error has occurred and has been logged.";
+        Assert.assertEquals(actualText,expectedText);
+
+    }
 }
